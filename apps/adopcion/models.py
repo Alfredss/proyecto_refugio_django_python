6f667d7424f168443c971081fe445ba1f61922a6
Alfredss <alfredss.24.7@gmail.com>
2016-11-07 22:45:46 -0500

    Creamos los modelos Mascota en la app mascota y el modelo Persona en la app adopcion; ademas creamos las migraciones y finalmente guardamos los cambios en la base de datos

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# Creamos el modelo Adopcion
+
+class Persona(models.Model):
+	nombre = models.CharField(max_length = 50)
+	apellidos = models.CharField(max_length = 50)
+	edad = models.IntegerField()
+	telefono = models.CharField(max_length = 12)
+	email = models.EmailField()
+	domicilio = models.TextField()
--- apps/mascota/models.py
+++ apps/mascota/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# Creamos nuestro modelo Mascota (debe de estar en singular)
+
+class Mascota(models.Model):
+	folio = models.CharField(max_length = 10, primary_key = True)
+	nombre = models.CharField(max_length=50)
+	sexo = models.CharField(max_length=10)
+	edad_aproximada = models.IntegerField()
+	fecha_rescate = models.DateField()
